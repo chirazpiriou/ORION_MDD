40478d6186acbf9a9a7e534ee6c2f9b88e0d2a7e
chiraz.piriou <chirazpiriou@gmail.com>
2025-01-31 22:16:53 +0100

    feat(back) -> Implement ArticleController for article endpoints

---
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/ArticleController.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.mddapi.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.openclassrooms.mddapi.dto.ArticleDTO;
+import com.openclassrooms.mddapi.services.Interfaces.IArticleService;
+
+@RestController
+@RequestMapping("/api/article")
+public class ArticleController {
+    @Autowired
+    private IArticleService articleService;
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<?> getArticleById(@PathVariable("id") Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).body(articleService.getArticleById(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllArticlesForUser(@RequestParam("userEmail") String userEmail) {
+        return ResponseEntity.status(HttpStatus.OK).body(articleService.getAllArticlesForUser(userEmail));
+    }
+
+    @ResponseBody
+    @PostMapping("/create")
+    public ResponseEntity<?> postArticle(@Valid @RequestBody ArticleDTO articleRequestDTO,
+            @RequestParam("userEmail") String userEmail) {
+
+        ArticleDTO articleResponseDTO = articleService.postArticle(articleRequestDTO, userEmail);
+        if (articleResponseDTO == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(articleResponseDTO);
+        } else {
+            return ResponseEntity.ok(articleResponseDTO);
+        }
+    }
+
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/AuthController.java
@@ -0,0 +1,5 @@
+package com.openclassrooms.mddapi.controllers;
+
+public class AuthController {
+    
+}
