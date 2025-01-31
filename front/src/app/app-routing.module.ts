0a9b862aeaf1ea96c9673e2a427634daefb3e883
chiraz.piriou <chirazpiriou@gmail.com>
2025-01-31 22:58:07 +0100

    feat(front) -> Update routing module with new routes for login, register, article list, and theme pages

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ArticleListComponent } from './pages/article-list/article-list.component';
+import { ThemesListComponent } from './pages/themes-list/themes-list.component';
+import { ArticleCreateComponent } from './pages/article-create/article-create.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register',component: RegisterComponent},
+  { path: 'article/all', component: ArticleListComponent},
+  { path: 'article/create', component: ArticleCreateComponent},
+  { path: 'themes', component: ThemesListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  
 })
 export class AppRoutingModule {}
