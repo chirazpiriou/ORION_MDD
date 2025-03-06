51a47b305ba33ed4cc7bd07884f8c3748eac1108
chiraz.piriou <chirazpiriou@gmail.com>
2025-03-06 19:59:42 +0100

    feat(front) -> Fix responsive layout, remove header from home, and back arrow from login and register components

--- front/src/app/core/components/header/header.component.ts
+++ front/src/app/core/components/header/header.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent  {
+  
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  shouldHideBackButton(): boolean {
+    return this.router.url === '/login' || this.router.url === '/register' || this.router.url === '/';
+  }
+  shouldHideHeader(): boolean {
+    return this.router.url === '/';
+  }
+  
+    
 
   back(): void {
     window.history.back();
